0391a1891db7869ab7465e46df0549e31396a082
mahdi nasiri <mmahdi.nasiri@yahoo.com>
2020-09-24 07:37:52 -0700

    change wizardform template to html way

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='users-login'),
     path('logout/', views.logout_view, name='users-logout'),
-    path('information/', login_required(views.FormWizardView.as_view(views.INFORMATION_WIZARD_FORMS)), name='users-information'),
+    # path('information/', login_required(views.FormWizardView.as_view(views.INFORMATION_WIZARD_FORMS)), name='users-information'),
     path('done/', views.done, name='users-done'),
+    path('form/', views.form_wizard, name='users-information'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import login, logout
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.core import paginator
 from django.core.exceptions import ObjectDoesNotExist
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.utils.decorators import method_decorator
@@ -38,88 +40,130 @@ def logout_view(request):
     return redirect('blog-home')
 
 
-INFORMATION_WIZARD_FORMS = (
-    ("FormStepOne", FormStepOne),
-    ("FormStepTwo", FormStepTwo),
-    ("FormStepThree", FormStepThree)
-)
 
 
-class FormWizardView(UserPassesTestMixin, SessionWizardView):
-    template_name = "users/wizardform.html"
-    instance = None
-    redirect_field_name = 'users/done.html'
-    initial = {}
-
-    def test_func(self):
-        print('test_func')
-        user = self.request.user
-        if not user.is_complete:
-            return True
-        return False
-
-    def handle_no_permission(self):
-        return redirect("users-done")
-
-    def get_form_instance(self, step):
-        if self.instance is None:
-            self.instance = Information(user=self.request.user)
-        return self.instance
-
-    def done(self, form_list, **kwargs):
-        FormStepOne, FormStepTwo, FormStepThree = form_list
-        project = self.instance
-        user = self.request.user
-        user.setcomplete(True)
-        user.save()
-        project.save()
+@login_required()
+def form_wizard(request):
+    user = request.user
+    try:
         user_information = Information.objects.get(user=user)
-        rand = random.random()
-        rand *= 100000
-        rand = round(rand)
-        return render(self.request, 'users/done.html', {
-            'data': user_information,
-            'random': rand
-        })
+    except ObjectDoesNotExist:
+        user_information = Information.objects.create(user=user)
 
-    def get_form_initial(self, step):
-        initial = self.initial
-        try:
-            userinformation = Information.objects.get(user=self.request.user)
-            initial.update(userinformation.__dict__)
-            return self.initial_dict.get(step, initial)
-        except ObjectDoesNotExist:
-            return self.initial_dict.get(step, {})
+    if request.method == 'GET':
+        form1 = FormStepOne(instance=user)
+        context = {
+            'form': form1
+        }
+    #
+    # if request.method == 'POST':
+    #
+    #     if 'firstName' in request.POST :
+    #         print('1')
+    #         form1 = FormStepOne(request.POST, instance=user)
+    #         context = {
+    #             'form': form1
+    #         }
+        # elif page == 2:
+        #     form2 = FormStepTwo(request.POST, instance=user)
+        #     context = {
+        #         'form': form2
+        #     }
+        # elif page == 3:
+        #     form3 = FormStepThree(request.POST, instance=user)
+        #     context = {
+        #         'form': form3
+        #     }
+    return render(request, 'users/wizardform.html', context)
 
-    def post(self, *args, **kwargs):
-        print('def post')
-        step = self.steps.current
-        print(step)
-        form = self.get_form(data=self.request.POST, files=self.request.FILES)
 
-        if form.is_valid():
-            if step == 'FormStepOne':
-                project = self.instance
-                project.save()
-            elif step == 'FormStepTwo':
-                userinformation = Information.objects.filter(user=self.request.user)
-                education = form.cleaned_data['education']
-                field = form.cleaned_data['field']
-                university = form.cleaned_data['university']
-                studentNumber = form.cleaned_data['studentNumber']
-                religousEducation = form.cleaned_data['religousEducation']
-                englishLanguage = form.cleaned_data['englishLanguage']
-                arabicLanguage = form.cleaned_data['arabicLanguage']
+    # INFORMATION_WIZARD_FORMS = (
+    #     ("FormStepOne", form1),
+    #     ("FormStepTwo", form2),
+    #     ("FormStepThree", form3)
+    # )
+    # paginator = Paginator(INFORMATION_WIZARD_FORMS, 1)
+    #
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
+    # return render(request, 'users/wizardform.html', {'page_obj': page_obj})
 
-                userinformation.update(education=education)
-                userinformation.update(field=field)
-                userinformation.update(university=university)
-                userinformation.update(studentNumber=studentNumber)
-                userinformation.update(religousEducation=religousEducation)
-                userinformation.update(englishLanguage=englishLanguage)
-                userinformation.update(arabicLanguage=arabicLanguage)
 
-        return super(FormWizardView, self).post(*args, **kwargs)
+# class FormWizardView(UserPassesTestMixin, SessionWizardView):
+#     template_name = "users/wizardform.html"
+#     instance = None
+#     redirect_field_name = 'users/done.html'
+#     initial = {}
+#
+#     def test_func(self):
+#         print('test_func')
+#         user = self.request.user
+#         if not user.is_complete:
+#             return True
+#         return False
+#
+#     def handle_no_permission(self):
+#         return redirect("users-done")
+#
+#     def get_form_instance(self, step):
+#         if self.instance is None:
+#             self.instance = Information(user=self.request.user)
+#         return self.instance
+#
+#     def done(self, form_list, **kwargs):
+#         FormStepOne, FormStepTwo, FormStepThree = form_list
+#         project = self.instance
+#         user = self.request.user
+#         user.setcomplete(True)
+#         user.save()
+#         project.save()
+#         user_information = Information.objects.get(user=user)
+#         rand = random.random()
+#         rand *= 100000
+#         rand = round(rand)
+#         return render(self.request, 'users/done.html', {
+#             'data': user_information,
+#             'random': rand
+#         })
+#
+#     def get_form_initial(self, step):
+#         initial = self.initial
+#         try:
+#             userinformation = Information.objects.get(user=self.request.user)
+#             initial.update(userinformation.__dict__)
+#             return self.initial_dict.get(step, initial)
+#         except ObjectDoesNotExist:
+#             return self.initial_dict.get(step, {})
+#
+#     def post(self, *args, **kwargs):
+#         print('def post')
+#         step = self.steps.current
+#         print(step)
+#         form = self.get_form(data=self.request.POST, files=self.request.FILES)
+#
+#         if form.is_valid():
+#             if step == 'FormStepOne':
+#                 project = self.instance
+#                 project.save()
+#             elif step == 'FormStepTwo':
+#                 userinformation = Information.objects.filter(user=self.request.user)
+#                 education = form.cleaned_data['education']
+#                 field = form.cleaned_data['field']
+#                 university = form.cleaned_data['university']
+#                 studentNumber = form.cleaned_data['studentNumber']
+#                 religousEducation = form.cleaned_data['religousEducation']
+#                 englishLanguage = form.cleaned_data['englishLanguage']
+#                 arabicLanguage = form.cleaned_data['arabicLanguage']
+#
+#                 userinformation.update(education=education)
+#                 userinformation.update(field=field)
+#                 userinformation.update(university=university)
+#                 userinformation.update(studentNumber=studentNumber)
+#                 userinformation.update(religousEducation=religousEducation)
+#                 userinformation.update(englishLanguage=englishLanguage)
+#                 userinformation.update(arabicLanguage=arabicLanguage)
+#
+#         return super(FormWizardView, self).post(*args, **kwargs)
 
 
 def done(request):
